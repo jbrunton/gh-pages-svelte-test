c048de7d6ad7980dd56e2b6b2d9b02c2d04ca43c
John Brunton <john_brunton@live.co.uk>
2021-04-18 17:39:51 +0100

    add deploy script

---
+++ scripts/deploy.js
@@ -0,0 +1,23 @@
+/* eslint-disable no-console */
+const execa = require("execa");
+const fs = require("fs");
+(async () => {
+  try {
+    await execa("git", ["checkout", "--orphan", "gh-pages"]);
+    // eslint-disable-next-line no-console
+    console.log("Building started...");
+    await execa("npm", ["run", "build"]);
+    await execa("git", ["--work-tree", "public", "add", "--all"]);
+    await execa("git", ["--work-tree", "public", "commit", "-m", "gh-pages"]);
+    console.log("Pushing to gh-pages...");
+    await execa("git", ["push", "origin", "HEAD:gh-pages", "--force"]);
+    await execa("rm", ["-r", "public"]);
+    await execa("git", ["checkout", "-f", "develop"]);
+    await execa("git", ["branch", "-D", "gh-pages"]);
+    console.log("Successfully deployed, check your settings");
+  } catch (e) {
+    // eslint-disable-next-line no-console
+    console.log(e.message);
+    process.exit(1);
+  }
+})();
